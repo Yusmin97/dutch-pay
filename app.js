e8828ca47c50026d5370de2898fb0c1d45893005
Yusmin97 <tmdamsi97@gmail.com>
2023-10-24 16:41:49 +0900

    서버 생성기 코드 작성

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hello')
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      fs.readFile('index.html', 'utf8', (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('서버 에러');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data);
+        }
+      });
+    }
+  }
+});
