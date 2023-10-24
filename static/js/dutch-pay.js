ab8c1ed368dd5a72ad02fa6b1da9f72390b8c42d
Yusmin97 <tmdamsi97@gmail.com>
2023-10-24 18:35:28 +0900

    더치페이 함수 버튼 이벤트 추가 후 id값을 정수로 추출하게 지정

---
+++ static/js/dutch-pay.js
@@ -0,0 +1,8 @@
+document.getElementById('calculator-btn').addEventListener('click', () => {
+  let mainFood = parseInt(document.getElementById('main-food').value)
+  let totalPeople = parseInt(document.getElementById('total-people').value)
+  let alcoholPeople = parseInt(document.getElementById('alcohol-people').value)
+  let alcoholCost = parseInt(document.getElementById('alcoho-cost').value)
+  let drinkPeople = parseInt(document.getElementById('drink-people').value)
+  let drinkCost = parseInt(document.getElementById('drink-cost').value)
+})
