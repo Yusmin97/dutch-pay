87ae98f23d3125d20d2a96a65cfabc320ab8eaa3
Yusmin97 <tmdamsi97@gmail.com>
2023-10-24 22:33:59 +0900

    dutch-pay.js readfile 지정

--- app.js
+++ app.js
@@ -33,6 +33,16 @@ const server = http.createServer((req, res) => {
           res.end(data);
         }
       });
+    } else if (req.url === '/static/js/dutch-pay.js') {
+      fs.readFile('./static/js/dutch-pay.js', 'utf8', (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('서버 에러');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'Application/javascript' });
+          res.end(data);
+        }
+      });
     } else {
       res.writeHead(404, {'Content-Type' : 'text/plain'})
       res.end('Not Found')
