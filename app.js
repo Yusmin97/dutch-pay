be684b7add327179e458f65e6358995e256da2b5
Yusmin97 <tmdamsi97@gmail.com>
2023-10-25 12:24:28 +0900

    index.html readfile 경로 재지정

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
     if (req.url === '/') {
-      fs.readFile('index.html', 'utf8', (err, data) => {
+      fs.readFile('./static/index.html', 'utf8', (err, data) => {
         if (err) {
           res.writeHead(500, { 'Content-Type': 'text/plain' });
           res.end('서버 에러');
